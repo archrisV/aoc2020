0a91671ae517096a58afccd25af33eea9a36edbb
Christian Torbica <uplink.chris@gmail.com>
2020-12-01 19:59:17 +0100

    Solved day1 part1

--- day1/DayOne.cs
+++ day1/DayOne.cs
@@ -1,13 +1,31 @@
 using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
 
 namespace day1
 {
     public class DayOne
     {
+        public const int targetYear = 2020;
+        private static List<int> ParseInput() => File.ReadAllText(".\\input.txt")
+                                                    .Split("\n")
+                                                    .Select(x => Int32.Parse(x))
+                                                    .ToList();
 
         public static void Main(string[] args)
         {
-
+            var list = ParseInput();
+            long target = 0;
+            foreach (var x in list)
+            {
+                if(list.Contains(targetYear - x))
+                {
+                    target = (targetYear - x) * x;
+                    break;
+                }
+            }
+            Console.WriteLine(target);
         }
     }
 }
