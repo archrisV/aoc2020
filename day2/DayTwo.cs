798eadab1f839976b9fc67e177acc714da27f3f7
Christian Torbica <uplink.chris@gmail.com>
2020-12-02 11:55:13 +0100

    Solution to part 2 day 2

--- day2/DayTwo.cs
+++ day2/DayTwo.cs
@@ -28,9 +28,29 @@ namespace day2
             return validPasswords;
         }
 
+        public static int SolutionPartTwo(List<string> list)
+        {
+            var validPasswords = 0;
+            foreach(var dataset in list)
+            {
+                var currentDataset = dataset.Replace(":", "").Split(" ").ToList();
+                var firstIndex = Int32.Parse(currentDataset.First().Split("-").First());
+                var secondIndex = Int32.Parse(currentDataset.First().Split("-").Last());
+                if(currentDataset.Last()[firstIndex - 1].ToString() == currentDataset[1] && 
+                    currentDataset.Last()[secondIndex - 1].ToString() != currentDataset[1] ||
+                    currentDataset.Last()[firstIndex - 1].ToString() != currentDataset[1] && 
+                    currentDataset.Last()[secondIndex - 1].ToString() == currentDataset[1])
+                {
+                    ++validPasswords;
+                }
+            }
+            return validPasswords;
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine(SolutionPartOne(ParseInput()));
+            Console.WriteLine($"Part 1 solution: {SolutionPartOne(ParseInput())}");
+            Console.WriteLine($"Part 2 solution: {SolutionPartTwo(ParseInput())}");
         }
     }
 }
--- day2/DayTwoTest.cs
+++ day2/DayTwoTest.cs
@@ -9,5 +9,11 @@ namespace day2
         {
             Assert.Equal(2, DayTwo.SolutionPartOne(new List<string>() { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc" }));
         }
+
+        [Fact]
+        public void Solution_Part_Two_Provided_Test()
+        {
+            Assert.Equal(1, DayTwo.SolutionPartTwo(new List<string>() { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc" }));
+        }
     }
 }
