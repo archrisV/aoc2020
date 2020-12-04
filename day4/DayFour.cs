97cbb7ce18e43f5f06aac12c14e6388557ce1289
Christian Torbica <uplink.chris@gmail.com>
2020-12-04 11:49:35 +0100

    Part 1 solution

--- day4/DayFour.cs
+++ day4/DayFour.cs
@@ -8,12 +8,22 @@ namespace day4
     public class DayFour
     {
         private static List<string> ParseInput() => File.ReadAllText(".\\input.txt")
-                                                    .Split("\n\n")
+                                                    .Split("\r\n\r\n")
                                                     .ToList();
 
         public static int SolutionPartOne(List<string> list)
         {
-
+            var keys = new List<string>(){ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+            var validPassports = 0;
+            list.ForEach(x => x.Replace("\n", " "));
+            foreach(var passport in list)
+            {
+                if(keys.Where(x => passport.Contains(x)).Count() == 7)
+                {
+                    validPassports++;
+                }
+            }
+            return validPassports;
         }
 
         /*public static int SolutionPartTwo(List<string> list)
